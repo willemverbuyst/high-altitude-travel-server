544b2d429fb6af86e4ad5928b499bb3d15e55b05
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-26 16:25:13 +0200

    Set up server with Express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const express = require('express');
+const { PORT } = require('./config/constants');
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
