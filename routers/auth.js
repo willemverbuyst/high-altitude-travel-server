ce2d4b487adeb778f157de37aeab79e7a5ec791b
Willem Verbuyst <willemverbuyst@gmail.com>
2020-07-27 05:28:30 +0200

    Create router for login

--- routers/auth.js
+++ routers/auth.js
@@ -6,6 +6,35 @@ const User = require('../models').user;
 
 const router = new Router();
 
+/*** LOGIN ***/
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: 'Please provide both email and password' });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: 'User with that email not found or password incorrect',
+      });
+    }
+
+    delete user.dataValues['password']; // don't send back the password hash
+    const token = toJWT({ userId: user.id });
+
+    return res.status(200).send({ token, ...user.dataValues });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: 'Something went wrong, sorry' });
+  }
+});
+
 /*** SIGNUP ***/
 router.post('/signup', async (req, res) => {
   const { name, email, password } = req.body;
